7c33cf50e57206be938784ecf01c571d779a5bd4
pjarma <hp@topfinance.pl>
2026-08-18 18:42:59 +0200

    commit 1

--- src/test/java/pl/course/customer/CustomerMapperTest.java
+++ src/test/java/pl/course/customer/CustomerMapperTest.java
@@ -2,6 +2,8 @@ package pl.course.customer;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CustomerMapperTest {
@@ -10,7 +12,7 @@ class CustomerMapperTest {
 
     @Test
     void shouldMapCustomerToDto() {
-        Customer customer = new Customer(1L, "Anna", "Nowak", "anna.nowak@example.com");
+        Customer customer = new Customer(1L, "Anna", "Nowak", "anna.nowak@example.com", LocalDate.parse("2026-08-18"));
 
         CustomerDto dto = mapper.toDto(customer);
 
@@ -18,6 +20,7 @@ class CustomerMapperTest {
         assertEquals("Anna", dto.firstName());
         assertEquals("Nowak", dto.lastName());
         assertEquals("anna.nowak@example.com", dto.email());
+        assertEquals(LocalDate.parse("2026-08-18"), dto.dateOfBirth());
     }
 }
 
