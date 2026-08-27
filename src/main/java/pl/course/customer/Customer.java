0e945858aa891bd299f539da6d832997ead44356
loczyn <loczyn@gmail.com>
2026-08-27 19:31:45 +0200

    Osoba B dodane

--- src/main/java/pl/course/customer/Customer.java
+++ src/main/java/pl/course/customer/Customer.java
@@ -1,17 +1,26 @@
 package pl.course.customer;
 
+import java.time.LocalDate;
+
 class Customer {
 
     private final Long id;
     private final String firstName;
     private final String lastName;
     private final String email;
+    private final LocalDate dateOfBirth;
 
-    Customer(Long id, String firstName, String lastName, String email) {
+    Customer(Long id, String firstName, String lastName, String email, LocalDate dateOfBirth) {
+        if (dateOfBirth == null || dateOfBirth.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("Invalid date of birth");
+        }
+
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.dateOfBirth = dateOfBirth;
+
     }
 
     Long getId() {
@@ -28,6 +37,10 @@ class Customer {
 
     String getEmail() {
         return email;
+    }
+
+    LocalDate getDateOfBirth() {
+        return dateOfBirth;
     }
 }
 
--- src/test/java/pl/course/customer/CustomerMapperTest.java
+++ src/test/java/pl/course/customer/CustomerMapperTest.java
@@ -2,6 +2,8 @@ package pl.course.customer;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CustomerMapperTest {
@@ -10,7 +12,8 @@ class CustomerMapperTest {
 
     @Test
     void shouldMapCustomerToDto() {
-        Customer customer = new Customer(1L, "Anna", "Nowak", "anna.nowak@example.com");
+
+        Customer customer = new Customer(1L, "Anna", "Nowak", "anna.nowak@example.com", LocalDate.of(1990, 5, 20));
 
         CustomerDto dto = mapper.toDto(customer);
 
@@ -18,6 +21,7 @@ class CustomerMapperTest {
         assertEquals("Anna", dto.firstName());
         assertEquals("Nowak", dto.lastName());
         assertEquals("anna.nowak@example.com", dto.email());
+        assertEquals(LocalDate.of(1990, 5, 20), dto.dateOfBirth());
     }
 }
 
