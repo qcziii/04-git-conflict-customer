b1de1f4d8b23438a2e2f755985f83ddbbd332752
Bogusław <boguslawidzik@gmail.com>
2026-09-02 17:17:35 +0200

    feature/customer-phone-number

--- src/test/java/pl/course/customer/CustomerMapperTest.java
+++ src/test/java/pl/course/customer/CustomerMapperTest.java
@@ -10,7 +10,7 @@ class CustomerMapperTest {
 
     @Test
     void shouldMapCustomerToDto() {
-        Customer customer = new Customer(1L, "Anna", "Nowak", "anna.nowak@example.com");
+        Customer customer = new Customer(1L, "Anna", "Nowak", "anna.nowak@example.com", "123456789");
 
         CustomerDto dto = mapper.toDto(customer);
 
@@ -18,6 +18,7 @@ class CustomerMapperTest {
         assertEquals("Anna", dto.firstName());
         assertEquals("Nowak", dto.lastName());
         assertEquals("anna.nowak@example.com", dto.email());
+        assertEquals("123456789", dto.phoneNumber());
     }
 }
 
