868f42dc2cee9b0283bca606caf6cc39f56d7b3b
iwowej <iwo.wejner@gmail.com>
2026-09-02 18:13:08 +0200

    added field dateOfBirth to Customer class, updated CustomerMapper and… (#16)

--- src/main/java/pl/course/customer/CustomerValidator.java
+++ src/main/java/pl/course/customer/CustomerValidator.java
@@ -1,10 +1,24 @@
 package pl.course.customer;
 
+import java.time.LocalDate;
+
 class CustomerValidator {
 
     void validateEmail(String email) {
         if (email == null || !email.contains("@")) {
             throw new IllegalArgumentException("Email jest niepoprawny");
+        }
+    }
+
+    void validateAge(LocalDate date) {
+        if (date == null) {
+            throw new IllegalArgumentException("Data urodzenia nie może być nullem.");
+        }
+
+        LocalDate now = LocalDate.now();
+
+        if (date.isAfter(now)) {
+            throw new IllegalArgumentException("Data urodzenia nie może być z przyszłości.");
         }
     }
 }
