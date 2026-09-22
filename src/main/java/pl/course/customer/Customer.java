11cb3a9857eadcd5c7fe3f2e85ba4a568355f54d
mikolajchylinski <chylinskimikolaj90@gmail.com>
2026-09-22 19:12:28 +0200

    added phoneNumber to Customer & CustomerDto

--- src/main/java/pl/course/customer/Customer.java
+++ src/main/java/pl/course/customer/Customer.java
@@ -6,12 +6,14 @@ class Customer {
     private final String firstName;
     private final String lastName;
     private final String email;
+    private final String phoneNumber;
 
-    Customer(Long id, String firstName, String lastName, String email) {
+     Customer(Long id, String firstName, String lastName, String email, String phoneNumber) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     Long getId() {
@@ -29,5 +31,7 @@ class Customer {
     String getEmail() {
         return email;
     }
+
+    String getPhoneNumber() {return phoneNumber;}
 }
 
